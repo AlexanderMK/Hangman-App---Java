c816212ad4126740e5859263a3bd52a615423321
Alexander <mburukungu@gmail.com>
2017-08-04 16:18:57 +0300

    made it illegal to repeat a guesse

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,9 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
+public class Hangman {
+  private String answer;
+  private String hits;
+  private String misses;
+  public static final int NUMBER_OF_TRIES = 7;
 
-public class PingPong {
 
-  
+  public Hangman(String answer) {
+        this.answer = answer;
+        hits = "";
+        misses = "";
+    }
+
+  public boolean applyGuess(char c) {
+       boolean isCorrect = false;
+       if (hits.indexOf(c) != -1 || misses.indexOf(c) != -1) {
+           throw new IllegalArgumentException("You already made this guess ");
+       }
+       if (answer.indexOf(c) != -1) {
+           isCorrect = true;
+           hits += c;
+       } else {
+           misses += c;
+       }
+       return isCorrect;
   }
 
 }
