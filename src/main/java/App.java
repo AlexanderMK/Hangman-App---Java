4706235d9caae22cb06d2b60b603a6b6a37a692b
Alexander <mburukungu@gmail.com>
2017-08-04 15:33:12 +0300

    then created a string of words

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ public class App {
   public static void main(String[] args) {
     List<Character> guess = new Arraylist<>();
     Random randomNumber = new Random();
+    String[] words = { "collins", "cow", "chicken", "pizza", "bread", "stones","sticks", "leaves", "letters", "alphabet", "soup", "hungry", "tired", "sleepy", "noisy", "caring", "friends", "month" };
 
 
     System.out.println("Welcome to Hangman Application!");
