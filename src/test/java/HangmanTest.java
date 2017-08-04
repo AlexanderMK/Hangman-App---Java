e1230adcbc2c9cdd46a6cf7c05c25dd07e14af2c
Alexander <mburukungu@gmail.com>
2017-08-04 15:46:29 +0300

    Test1 - Choose a char whose answer will match the word guessed

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -3,4 +3,9 @@ import static org.junit.Assert.*;
 
 public class HangmanTest {
 
+  hangman hangman
+  @Test
+    public void choosingACharacterInTheAnswerReturnsTrue() throws Exception {
+        assertTrue(hangman.applyGuess('c'));
+    }
 }
