fa696c8ecb64acdd21b437e44b8b2060d3d23393
Alexander <mburukungu@gmail.com>
2017-08-04 15:31:32 +0300

    placed random class to generate pseudo chars

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,8 @@ import java util.*;
 
 public class App {
   public static void main(String[] args) {
-    
+    List<Character> guess = new Arraylist<>();
+    Random randomNumber = new Random();
 
 
     System.out.println("Welcome to Hangman Application!");
